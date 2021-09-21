d5e0d0830faa8a2d7b155314dc6fdef82085e7ff
Aruna <aruna.evan15@gmail.com>
2021-09-21 15:00:54 -0700

    Aruna: delared functions returnFirstTwoDrivers, returnLastTwoDrivers, and createFareMuliplier. Delcared selectingDrivers.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-// Code your solution in this file!
-Test!
+const returnFirstTwoDrivers = function(myArray) {
+    return myArray.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(myArray) {
+    // return [myArray[myArray.length-2],myArray[myArray.length-1]];
+    return myArray.slice(myArray.length-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return (fare) => {
+        return int*fare;
+    }
+}
+
